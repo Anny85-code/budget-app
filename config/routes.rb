ee3e20099d5dd799f72fef365921ca1008dedfa5
uae <aniekaudo@yahoo.com>
2022-05-23 16:48:23 +0100

    :zap: Authentication
    Implemented authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'entities/show'
   get 'groups/index'
   get 'groups/show'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
