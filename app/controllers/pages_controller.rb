aa0ec5b1985209aee0c76179f57dab11febf10b0
uae <aniekaudo@yahoo.com>
2022-05-23 16:12:32 +0100

    :zap: Route
    Edit routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+    @message = "This is the home page"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   get 'entities/index'
   get 'entities/show'
   get 'groups/index'
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
