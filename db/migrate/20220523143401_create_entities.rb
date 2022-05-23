9f4f12c401d075e72b986cecd0888f2a0cf464bb
uae <aniekaudo@yahoo.com>
2022-05-23 15:47:11 +0100

    :card_file_box: Tables
    Create entity and group tables

---
+++ db/migrate/20220523143401_create_entities.rb
@@ -0,0 +1,12 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.integer :author_id
+      t.string :name
+      t.float :amount
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_135539) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_143401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entities", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "name"
+    t.float "amount"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_entities_on_user_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -37,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_135539) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "users"
   add_foreign_key "groups", "users"
 end
