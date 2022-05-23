59bad2bf24b9b41716c085a1831552b3ecb931df
uae <aniekaudo@yahoo.com>
2022-05-23 16:00:00 +0100

    :card_file_box: Table
    Entity controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'entities/index'
+  get 'entities/show'
+  get 'groups/index'
+  get 'groups/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
