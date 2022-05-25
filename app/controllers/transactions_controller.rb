f554b929638bfdb8fd46e54ae99357a6d09d8517
uae <aniekaudo@yahoo.com>
2022-05-25 20:06:40 +0100

    :sparkles: Navbar
    Implement navbar

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,6 +5,7 @@ class TransactionsController < ApplicationController
   def index
     @transactions = Transaction.all
     @recent_transactions = @transactions.order(created_at: :desc)
+    @total_transactions = @transactions.sum(:amount)
  end
 
   # GET /transactions/1 or /transactions/1.json
