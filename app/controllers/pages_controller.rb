30d09e28317ea12241356b55f8fdf3a7f1989cf7
uae <aniekaudo@yahoo.com>
2022-05-26 23:14:02 +0100

    :coffin: Dead code removed dead coode
    Removed dead codes from pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,3 @@ class PagesController < ApplicationController
     @categories = Category.includes(:transactions).order(id: 'DESC')
   end
 end
-
-
-# @user = User.includes(:posts, :comments, :likes).find(params[:author_id])
-#     @posts = Post.all
-
-#      @foods = Recipe.includes(:recipe_foods).where(public: true).order(id: 'DESC')
