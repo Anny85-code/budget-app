7913868fe91c8266a042bc0775aa8e7077b69f9e
uae <aniekaudo@yahoo.com>
2022-05-27 15:02:20 +0100

    :test_tube: Test
    Added test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,48 +1,23 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   # before(:each) do
-#   #     User.destroy_all
-#   user = User.create(name: 'anny', email: 'example@gmail.com', password: '3344455')
-#   it 'create a user' do
-#     expect(user).to be_valid
-#   end
-# # end
-# end
+RSpec.describe User, type: :model do
+  describe 'Test for the User validation' do
+    subject { User.new(name: 'John', email: 'john@mail.com', password: 'john') }
+    before { subject.save }
 
+    it 'if there is name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
 
-# require 'rails_helper'
+    it 'if there is email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
 
-# RSpec.describe User, type: :model do
-#   subject { User.create(name: 'Blessing', surname: 'John', email: 'testtest@gmail.com', password: '444444444') }
-
-#   it 'Name must not be blank' do
-#     subject.name = nil
-#     expect(subject).to_not be_valid
-#   end
-
-#   it 'Surame must not be blank' do
-#     subject.surname = nil
-#     expect(subject).to_not be_valid
-#   end
-
-#   it 'Name must not be blank' do
-#     subject.name = 'Blessing'
-#     expect(subject).to be_valid
-#   end
-
-#   it 'Name must not be blank' do
-#     subject.surname = 'John'
-#     expect(subject).to be_valid
-#   end
-
-#   it 'email must not be blank' do
-#     subject.email = 'testtest@gmail.com'
-#     expect(subject).to be_valid
-#   end
-
-#   it 'password must not be blank' do
-#     subject.password = '444444444'
-#     expect(subject).to be_valid
-#   end
-# end
+    it 'if password length is under 6' do
+      subject.password = 1111
+      expect(subject).to_not be_valid
+    end
+  end
+end
