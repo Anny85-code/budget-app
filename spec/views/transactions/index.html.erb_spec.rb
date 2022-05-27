d5497f2edf5ff8a67127e18e7edada90feb7597c
uae <aniekaudo@yahoo.com>
2022-05-27 09:15:43 +0100

    :lipstick: Alert
    Update alert display style

--- spec/views/transactions/index.html.erb_spec.rb
+++ spec/views/transactions/index.html.erb_spec.rb
@@ -4,24 +4,24 @@ RSpec.describe "transactions/index", type: :view do
   before(:each) do
     assign(:transactions, [
       Transaction.create!(
-        name: "Name",
-        amount: "",
-        user: nil,
-        category: nil
+        name: "Money Transfer",
+        amount: "200.00",
+        user: 'Anny',
+        category: 'Microverse'
       ),
       Transaction.create!(
-        name: "Name",
-        amount: "",
-        user: nil,
-        category: nil
+        name: "Contribution",
+        amount: "400.00",
+        user: 'Anny',
+        category: 'Microverse'
       )
     ])
   end
 
   it "renders a list of transactions" do
     render
-    assert_select "tr>td", text: "Name".to_s, count: 2
-    assert_select "tr>td", text: "".to_s, count: 2
+    assert_select "tr>td", text: "Money Transfers".to_s, count: 2
+    assert_select "tr>td", text: "Contribution".to_s, count: 2
     assert_select "tr>td", text: nil.to_s, count: 2
     assert_select "tr>td", text: nil.to_s, count: 2
   end
