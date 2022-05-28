3fd14eafe1a68613ca690b47bcc2827f74a243e6
uae <aniekaudo@yahoo.com>
2022-05-28 08:12:11 +0100

    :memo: Updated readme
    Updated readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :categories,  dependent: :destroy
-  has_many :transactions,  dependent: :destroy
+  has_many :categories, dependent: :destroy
+  has_many :transactions, dependent: :destroy
 end
